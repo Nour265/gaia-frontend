02695c30876efb2dca23494b8bd5fc99d23c764c
BahaaMattar <bahaa.kmattar@gmail.com>
2026-04-20 14:17:07 +0300

    feat: Move build command to separate script for better maintainability

---
+++ gaia/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git clone https://github.com/flutter/flutter.git -b stable --depth 1 /tmp/flutter
+export PATH="$PATH:/tmp/flutter/bin"
+flutter pub get
+flutter build web --release \
+  --dart-define=API_BASE_URL=$API_BASE_URL \
+  --dart-define=GAIA_GOOGLE_CLIENT_ID=$GAIA_GOOGLE_CLIENT_ID
